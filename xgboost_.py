0f52c2288a1155ccd74aa59498a782097edcbd8e
Kor-NOH <shehddnjs1982@naver.com>
2024-10-05 18:20:44 +0900

    Visualize the results

--- xgboost_.py
+++ xgboost_.py
@@ -1,6 +1,13 @@
 # Part 1 - 데이터 전처리
+import matplotlib
+matplotlib.use('TkAgg')
+
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+import xgboost as xgb
+import matplotlib.pyplot as plt
 
 # 데이터셋 불러오기
 dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
@@ -20,9 +27,6 @@ def create_dataset(data, time_step = 1):
 X, y = create_dataset(data.values, time_step=60)
 
 # 전체 데이터셋을 다시 분할 (학습 8: 테스트 2)
-from sklearn.model_selection import train_test_split
-import xgboost as xgb
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
 
 # XGBoost에 적용하기 위해 데이터를 DMatrix 형식으로 변환
@@ -31,11 +35,11 @@ dtest = xgb.DMatrix(X_test, label=y_test)
 
 # Part 2 - 모델 생성
 params = {
-    'objective' : 'reg:squarederror',
-    'colsample_bytree': 0.3,
-    'learning_rate': 0.1,
-    'max_depth': 5,
-    'alpha' : 10
+    'objective' : 'reg:squarederror',   # 손실 함수
+    'colsample_bytree': 0.3,    # 무작위로 선택할 특징(변수) 비율 설정 , 전체 특징중 30%만을 샘플링
+    'learning_rate': 0.1,   # 학습률
+    'max_depth': 5,     # 트리 최대 깊이
+    'alpha' : 10    # L1 정규화 항, 모델의 복잡성을 줄여 과적합 방지
 }
 
 # 모델 학습
@@ -44,9 +48,16 @@ model = xgb.train(params, dtrain, num_boost_round=100)
 # Part 3 - 예측
 predictions = model.predict(dtest)
 
-from sklearn.metrics import mean_squared_error
-
 # 평균제곱오차(MSE)를 사용하여 모델 평가
 mse = mean_squared_error(y_test, predictions)
 print(f'Mean Squared Error: {mse}')
 
+# Part 4 - 결과 시각화
+plt.figure(figsize=(12, 6))
+plt.plot(y_test, color='red', label='Real Stock Price')
+plt.plot(predictions, color='blue', label = 'Predicted Stock Price')
+plt.title('Google Stock Price Prediction using XGBoost')
+plt.xlabel('Time')
+plt.ylabel('Google Stock Price')
+plt.legend()
+plt.show()
